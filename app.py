22858b61e4589e3307934a16ee7d245f23511823
omn5143-a11y <omnarkhede5143@gmail.com>
2026-04-21 11:41:50 +0530

    Change home route response to 'hello cloud'

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-return "hello"
 
-if __name__ == "__main__":
-port = int(os.environ.get("PORT",5000))
-app.run(host='0.0.0.0',port=port)
+def home():
+
+    return "hello cloud"
+
+if name == "__main__":
+
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host='0.0.0.0', port=port)
